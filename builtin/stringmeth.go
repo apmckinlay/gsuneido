3deaafe8689a1dce4e6f690d89fd2859b96e9aa0
Andrew McKinlay <apmckinlay@gmail.com>
2018-12-01 11:45:51 -0600

    more string methods

--- builtin/stringmeth.go
+++ builtin/stringmeth.go
@@ -8,20 +8,37 @@ import (
 
 func init() {
 	StringMethods = Methods{
-		"Size": method0(func(this Value) Value {
-			return SuInt(len(this.ToStr()))
+		"Asc": method0(func(this Value) Value {
+			return SuInt(int(this.ToStr()[0]))
 		}),
-		"Lower": method0(func(this Value) Value {
-			return SuStr(strings.ToLower(this.ToStr()))
-		}),
-		"Upper": method0(func(this Value) Value {
-			return SuStr(strings.ToUpper(this.ToStr()))
+		"Find": method1("(string)", func(this, arg Value) Value {
+			s := this.ToStr()
+			i := strings.Index(s, arg.ToStr())
+			if i == -1 {
+				i = len(s)
+			}
+			return IntToValue(i)
 		}),
 		"Has?": method1("(string)", func(this, arg Value) Value {
 			return SuBool(strings.Contains(this.ToStr(), arg.ToStr()))
 		}),
+		"Lower": method0(func(this Value) Value {
+			return SuStr(strings.ToLower(this.ToStr()))
+		}),
+		"Prefix?": method1("(string)", func(this, arg Value) Value {
+			return SuBool(strings.HasPrefix(this.ToStr(), arg.ToStr()))
+		}),
+		"Suffix?": method1("(string)", func(this, arg Value) Value {
+			return SuBool(strings.HasSuffix(this.ToStr(), arg.ToStr()))
+		}),
 		"Repeat": method1("(count)", func(this, arg Value) Value {
 			return SuStr(strings.Repeat(this.ToStr(), arg.ToInt()))
+		}),
+		"Size": method0(func(this Value) Value {
+			return IntToValue(len(this.ToStr()))
+		}),
+		"Upper": method0(func(this Value) Value {
+			return SuStr(strings.ToUpper(this.ToStr()))
 		}),
 		// TODO more methods
 	}
