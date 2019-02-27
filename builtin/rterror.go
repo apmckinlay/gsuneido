95a9b7ef790ea3989c79f5c4967f253bf78d1aba
Andrew McKinlay <apmckinlay@gmail.com>
2019-02-27 16:58:45 -0600

    comment

--- builtin/rterror.go
+++ builtin/rterror.go
@@ -2,6 +2,7 @@ package builtin
 
 import . "github.com/apmckinlay/gsuneido/runtime"
 
+// cause a Go runtime error (for testing)
 var _ = builtin0("RuntimeError()",
 	func() Value {
 		var x []Value
