8a08938650ed5fd2fa3a84c80f8b628202c5238a
Andrew McKinlay <apmckinlay@gmail.com>
2019-03-12 19:09:37 -0600

    more built in functions

--- builtin/type.go
+++ builtin/type.go
@@ -6,3 +6,43 @@ var _ = builtin1("Type(value)",
 	func(arg Value) Value {
 		return SuStr(arg.TypeName())
 	})
+
+var _ = builtin1("Boolean?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Boolean")
+	})
+
+var _ = builtin1("Number?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Number")
+	})
+
+var _ = builtin1("String?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "String")
+	})
+
+var _ = builtin1("Date?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Date")
+	})
+
+var _ = builtin1("Object?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Object")
+	})
+
+var _ = builtin1("Record?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Record")
+	})
+
+var _ = builtin1("Class?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Class")
+	})
+
+var _ = builtin1("Function?(value)",
+	func(arg Value) Value {
+		return SuBool(arg.TypeName() == "Function")
+	})
