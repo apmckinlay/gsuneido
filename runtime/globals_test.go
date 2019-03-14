e307b5c7f2716c885aeab536adfe9f649df36dda
Andrew McKinlay <apmckinlay@gmail.com>
2019-03-14 07:44:46 -0600

    Update globals_test.go

--- runtime/globals_test.go
+++ runtime/globals_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func TestGlobals(t *testing.T) {
-	Assert(t).That(Global.Num("foo"), Equals(1))
-	Assert(t).That(Global.Num("foo"), Equals(1))
-	Assert(t).That(Global.Add("bar", nil), Equals(2))
-	Assert(t).That(Global.Num("bar"), Equals(2))
+	foo := Global.Num("foo")
+	Assert(t).That(Global.Num("foo"), Equals(foo))
+	Assert(t).That(Global.Add("bar", nil), Equals(foo+1))
+	Assert(t).That(Global.Num("bar"), Equals(foo+1))
 	Global.Add("baz", True)
 	Assert(t).That(func() { Global.Add("baz", False) }, Panics("duplicate"))
-	Assert(t).That(Global.Name(1), Equals("foo"))
-	Assert(t).That(Global.Name(2), Equals("bar"))
+	Assert(t).That(Global.Name(foo), Equals("foo"))
+	Assert(t).That(Global.Name(foo+1), Equals("bar"))
 }
 
 var V Value
 
 func BenchmarkBuffer(b *testing.B) {
-	Global.Num("foo")
-	Global.Num("bar")
+	f := Global.Num("foo")
+	g := Global.Num("bar")
 	for n := 0; n < b.N; n++ {
-		V = Global.Get(1)
-		V = Global.Get(2)
+		V = Global.Get(f)
+		V = Global.Get(g)
 	}
 }
