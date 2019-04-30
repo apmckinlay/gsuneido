b7e4ddd78162517df3178b0ad12c5024b4d01504
Andrew McKinlay <apmckinlay@gmail.com>
2019-04-30 11:10:47 -0600

    fix Timestamp to be unique

--- builtin/timestamp.go
+++ builtin/timestamp.go
@@ -2,7 +2,14 @@ package builtin
 
 import . "github.com/apmckinlay/gsuneido/runtime"
 
+var prevTimestamp SuDate
+
 var _ = builtin0("Timestamp()", func() Value {
-	//TODO client/server, guarantee unique value
-	return Now()
+	//TODO client/server, concurrency
+	t := Now()
+	if t.Equal(prevTimestamp) {
+		t = t.Plus(0, 0, 0, 0, 0, 0, 1)
+		prevTimestamp = t
+	}
+	return t
 })
