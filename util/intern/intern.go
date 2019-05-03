e69f67167f4894e6a395ab813f68ee23df665201
Andrew McKinlay <apmckinlay@gmail.com>
2019-05-03 17:01:45 -0600

    better version of intern
    but decided not to use it

--- util/intern/intern.go
+++ util/intern/intern.go
@@ -1,20 +1,126 @@
 // Package intern handles sharing common strings
 package intern
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"unsafe"
+
+	"github.com/apmckinlay/gsuneido/util/hash"
+)
 
 // redundant to store string as key and value
 // but no way to get key that matched lookup
 
-var table = make(map[string]string)
+// buf holds the actual data of the strings
+var buffer []byte
+
+// strings points into buffer
+var strings = []string{""} // hash set can't handle zero
+
+// hashes stores/caches the high bits of the string hashes
+// to speed up resizing the hash set
+var hashes = []uint16{0}
+
+var set hashSet16
+
 var lock sync.Mutex
 
-func Intern(s string) string {
+// Index returns the index a shared copy of a string,
+// adding it if not present.
+func Index(s string) uint16 {
 	lock.Lock()
 	defer lock.Unlock()
-	if x, ok := table[s]; ok {
-		return x
+	h := hash16(s)
+	pi := set.slotFor(s, h)
+	if *pi == 0 {
+		t := addStringToBuffer(s)
+		strings = append(strings, t)
+		hashes = append(hashes, h)
+		*pi = uint16(len(strings) - 1)
 	}
-	table[s] = s
-	return s
+	return *pi
+}
+
+func String(i uint16) string {
+	lock.Lock()
+	defer lock.Unlock()
+	return strings[i]
+}
+
+func addStringToBuffer(s string) string {
+	i := len(buffer)
+	buffer = append(buffer, s...)
+	p := buffer[i:]
+	return *(*string)(unsafe.Pointer(&p))
+}
+
+// hashSet16 stores a set of int16 indexes into strings
+type hashSet16 struct {
+	tbl      []uint16
+	size     int
+	capShift uint32
+	capMask  uint16
+}
+
+const bighop = 11 // should be prime
+
+func (hs *hashSet16) slotFor(s string, h uint16) *uint16 {
+	if hs.size >= len(hs.tbl)*5/8 { // roughly 60%
+		hs.grow()
+	}
+	return hs.slotFor_(s, h)
+}
+func (hs *hashSet16) slotFor_(s string, h uint16) *uint16 {
+	i := h >> hs.capShift
+	hops := 0
+	for hs.tbl[i] != 0 {
+		if strings[hs.tbl[i]] == s {
+			return &hs.tbl[i]
+		}
+		if hops < bighop {
+			i++
+		} else {
+			i += bighop
+		}
+		i &= hs.capMask
+		hops++
+	}
+	hs.size++
+	return &hs.tbl[i]
+}
+
+const phi32 = 2654435769
+
+func hash16(s string) uint16 {
+	h := hash.HashString(s)
+	return uint16((h * phi32) >> 16)
+}
+
+const initialSize = 2          //1024        // should be power of 2
+const initialCapShift = 16 - 1 //16 - 10 // must match initialSize
+const initialCapMask = 1
+
+func (hs *hashSet16) grow() {
+	if len(hs.tbl) == 0 {
+		hs.tbl = make([]uint16, initialSize)
+		hs.capShift = initialCapShift
+		hs.capMask = initialCapMask
+		return
+	}
+	hs.tbl = make([]uint16, len(hs.tbl)*2)
+	hs.size = 0
+	hs.capShift--
+	hs.capMask = hs.capMask<<1 + 1
+	for i, s := range strings {
+		*hs.slotFor(s, hashes[i]) = uint16(i)
+	}
+}
+
+func (hs *hashSet16) print() {
+	fmt.Println("size", hs.size, "cap", len(hs.tbl), "lim", len(hs.tbl)*5/8)
+	for _, x := range hs.tbl {
+		fmt.Printf("%v ", x)
+	}
+	fmt.Println()
 }
--- util/intern/intern_test.go
+++ util/intern/intern_test.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"runtime"
 	"testing"
+
+	. "github.com/apmckinlay/gsuneido/util/hamcrest"
 )
+
+func TestIntern(t *testing.T) {
+	Assert(t).That(Index("hello"), Equals(1))
+	Assert(t).That(Index("world"), Equals(2))
+
+	Assert(t).That(String(1), Equals("hello"))
+	Assert(t).That(String(2), Equals("world"))
+
+	Assert(t).That(Index("hello"), Equals(1))
+
+	for c := 'a'; c <= 'z'; c++ {
+		Index(string(c))
+	}
+}
 
 var List []string
 var S = "hello"
 var T = func() string { return "world" }
 
-func TestIntern(*testing.T) {
-	for n := 0; n < 100000; n++ {
+func TestMemory(*testing.T) {
+	for n := 0; n < 10000; n++ {
 		// List = append(List, S+T())
-		List = append(List, Intern(S+T()))
+		List = append(List, String(Index(S+T())))
 	}
 	runtime.GC()
 	var ms runtime.MemStats
