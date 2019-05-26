c7680ff375de2cc96f3bcac17d56b386b0b4b4ba
Andrew McKinlay <apmckinlay@gmail.com>
2019-05-26 16:09:36 -0600

    add System, Spawn, RunPiped

---
+++ builtin/runpiped.go
@@ -0,0 +1,201 @@
+package builtin
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	. "github.com/apmckinlay/gsuneido/runtime"
+	"github.com/apmckinlay/gsuneido/runtime/types"
+)
+
+var _ = builtin("RunPiped(command, block=false)",
+	func(t *Thread, args []Value) Value {
+		command := ToStr(args[0])
+		cmdargs := splitCommand(command)
+		cmd := exec.Command(cmdargs[0], cmdargs[1:]...)
+		in, err := cmd.StdinPipe()
+		pr, pw, err := os.Pipe()
+		if err != nil {
+			panic("RunPiped create pipe failed: " + err.Error())
+		}
+		cmd.Stdout = pw
+		cmd.Stderr = pw
+
+		err = cmd.Start()
+		if err != nil {
+			panic("Runpiped failed to start: " + err.Error())
+		}
+		pw.Close()
+
+		rp := &suRunPiped{command: command, cmd: cmd, w: in, r: pr}
+		if args[2] == False {
+			return rp
+		}
+		// block form
+		defer rp.close()
+		return t.CallWithArgs(args[1], rp)
+	})
+
+func splitCommand(s string) []string {
+	args := []string{}
+	for {
+		s = strings.TrimLeft(s, " \t")
+		if s == "" {
+			return args
+		}
+		delim := byte(' ')
+		if s[0] == '"' {
+			delim = '"'
+			s = s[1:]
+		}
+		i := strings.IndexByte(s, delim)
+		if i == -1 {
+			return append(args, s)
+		}
+		args = append(args, s[:i])
+		s = s[i+1:]
+	}
+}
+
+type suRunPiped struct {
+	CantConvert
+	command string
+	cmd     *exec.Cmd
+	w       io.WriteCloser
+	r       io.ReadCloser
+}
+
+func (rp *suRunPiped) close() {
+	rp.r.Close()
+}
+
+// Value ------------------------------------------------------------
+
+var _ Value = (*suRunPiped)(nil)
+
+func (*suRunPiped) Get(*Thread, Value) Value {
+	panic("File does not support get")
+}
+
+func (*suRunPiped) Put(*Thread, Value, Value) {
+	panic("File does not support put")
+}
+
+func (*suRunPiped) RangeTo(int, int) Value {
+	panic("File does not support range")
+}
+
+func (*suRunPiped) RangeLen(int, int) Value {
+	panic("File does not support range")
+}
+
+func (*suRunPiped) Hash() uint32 {
+	panic("File hash not implemented")
+}
+
+func (*suRunPiped) Hash2() uint32 {
+	panic("File hash not implemented")
+}
+
+func (*suRunPiped) Compare(Value) int {
+	panic("File compare not implemented")
+}
+
+func (*suRunPiped) Call(*Thread, *ArgSpec) Value {
+	panic("can't call File")
+}
+
+func (rp *suRunPiped) String() string {
+	return "RunPiped(" + rp.command + ")"
+}
+
+func (*suRunPiped) Type() types.Type {
+	return types.File
+}
+
+func (rp *suRunPiped) Equal(other interface{}) bool {
+	if rp2, ok := other.(*suRunPiped); ok {
+		return rp == rp2
+	}
+	return false
+}
+
+func (*suRunPiped) Lookup(_ *Thread, method string) Callable {
+	return suRunPipedMethods[method]
+}
+
+var suRunPipedMethods = Methods{
+	"Close": method0(func(this Value) Value {
+		this.(*suRunPiped).close()
+		return this
+	}),
+	"CloseWrite": method0(func(this Value) Value {
+		this.(*suRunPiped).w.Close()
+		return this
+	}),
+	"ExitValue": method0(func(this Value) Value {
+		cmd := this.(*suRunPiped).cmd
+		err := cmd.Wait()
+		if err != nil {
+			if _, ok := err.(*exec.ExitError); !ok {
+				panic("System failed to run: " + err.Error())
+			}
+		}
+		return IntVal(cmd.ProcessState.ExitCode())
+	}),
+	"Flush": method0(func(this Value) Value {
+		return nil
+	}),
+	"Read": method1("(nbytes=1024)", func(this, arg Value) Value {
+		f := this.(*suRunPiped).r
+		n := IfInt(arg)
+		buf := make([]byte, n)
+		m, err := f.Read(buf)
+		if err != nil {
+			panic("runpiped.Read " + err.Error())
+		}
+		return SuStr(string(buf[:m]))
+	}),
+	"Readline": method0(func(this Value) Value {
+		f := this.(*suRunPiped).r
+		var buf strings.Builder
+		b := make([]byte, 1)
+		for {
+			n, err := f.Read(b)
+			if n == 0 {
+				if buf.Len() == 0 {
+					return False
+				}
+				break
+			}
+			if err != nil {
+				panic("runpiped.Readline failed " + err.Error())
+			}
+			if b[0] == '\n' {
+				break
+			}
+			if buf.Len() < MaxLine {
+				buf.WriteByte(b[0])
+			}
+		}
+		s := buf.String()
+		s = strings.TrimRight(s, "\r")
+		return SuStr(s)
+	}),
+	"Write": method1("(string)", func(this, arg Value) Value {
+		_, err := io.WriteString(this.(*suRunPiped).w, AsStr(arg))
+		if err != nil {
+			panic("runpiped.Write failed " + err.Error())
+		}
+		return arg
+	}),
+	"Writeline": method1("(string)", func(this, arg Value) Value {
+		_, err := io.WriteString(this.(*suRunPiped).w, AsStr(arg)+"\n")
+		if err != nil {
+			panic("runpiped.Writeline failed " + err.Error())
+		}
+		return arg
+	}),
+}
---
+++ builtin/spawn.go
@@ -0,0 +1,70 @@
+package builtin
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+
+	. "github.com/apmckinlay/gsuneido/runtime"
+)
+
+var _ = builtin1("System(command)",
+	func(arg Value) Value {
+		shell, name, flag := getShell()
+		if shell == "" {
+			panic("System: can't get " + name)
+		}
+		cmd := exec.Command(shell, flag, ToStr(arg))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			if _, ok := err.(*exec.ExitError); !ok {
+				panic("System failed to run: " + err.Error())
+			}
+		}
+		return IntVal(cmd.ProcessState.ExitCode())
+	})
+
+func getShell() (string, string, string) {
+	if runtime.GOOS == "windows" {
+		return os.Getenv("COMSPEC"), "COMSPEC", "/c"
+	}
+	return os.Getenv("SHELL"), "SHELL", "-c"
+}
+
+var _ = builtinRaw("Spawn(@args)",
+	func(t *Thread, as *ArgSpec, args []Value) Value {
+		const wait = 0
+		iter := NewArgsIter(as, args)
+		args = args[:0]
+		for k, v := iter(); k == nil && v != nil; k, v = iter() {
+			args = append(args, v)
+		}
+		if len(args) < 2 {
+			panic("usage: Spawn(mode, command, @args)")
+		}
+		mode := IfInt(args[0])
+		exe := ToStr(args[1])
+		var argstr []string
+		for _, v := range args[2:] {
+			argstr = append(argstr, ToStr(v))
+		}
+		cmd := exec.Command(exe, argstr...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Start()
+		if err != nil {
+			panic("Spawn failed to start: " + err.Error())
+		}
+		if mode == wait {
+			err = cmd.Wait()
+			if err != nil {
+				if _, ok := err.(*exec.ExitError); !ok {
+					panic("Spawn failed: " + err.Error())
+				}
+			}
+			return IntVal(cmd.ProcessState.ExitCode())
+		}
+		return nil
+	})
