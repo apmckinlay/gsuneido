14effb149b600fa1ee7b43d63fcb143fe7f06cf5
Andrew McKinlay <apmckinlay@gmail.com>
2019-05-27 17:04:26 -0600

    Create gsuneido_test.go

---
+++ gsuneido_test.go
@@ -0,0 +1,18 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	_ "github.com/apmckinlay/gsuneido/builtin"
+// 	"github.com/apmckinlay/gsuneido/database/dbms"
+// 	. "github.com/apmckinlay/gsuneido/runtime"
+// )
+
+// func TestGsuneido(t *testing.T) {
+// 	Global.Builtin("Suneido", new(SuObject))
+// 	GetDbms = func() IDbms { return dbms.NewDbmsClient("127.0.0.1:3147") }
+// 	Libload = libload // dependency injection
+// 	mainThread = NewThread()
+// 	defer mainThread.Close()
+// 	eval("FormulaContains_Test.Debug()")
+// }
