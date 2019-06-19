9648ba4240a10946e0af0601d7c91aced3322d30
Andrew McKinlay <apmckinlay@gmail.com>
2019-06-18 18:00:19 -0600

    seed Random from time

--- builtin/random.go
+++ builtin/random.go
@@ -2,9 +2,14 @@ package builtin
 
 import (
 	"math/rand"
+	"time"
 
 	. "github.com/apmckinlay/gsuneido/runtime"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 var _ = builtin1("Random(limit)", func(arg Value) Value {
 	limit := IfInt(arg)
