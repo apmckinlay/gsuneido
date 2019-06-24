2d2dac4feb54b71c8796177ea948784d4a69646c
Andrew McKinlay <apmckinlay@gmail.com>
2019-06-24 16:45:13 -0600

    fix Synchronized to be reentrant

--- builtin/synchronized.go
+++ builtin/synchronized.go
@@ -6,11 +6,35 @@ import (
 	. "github.com/apmckinlay/gsuneido/runtime"
 )
 
-var lock sync.Mutex
+// Synchronized is used recursively
+// and Go doesn't have a reentrant mutex
+// so we have to implement our own
+
+type synchInfo struct {
+	threadLock sync.Mutex
+	lockThread *Thread
+	lock       sync.Mutex
+}
+
+var sy synchInfo
 
 var _ = builtin("Synchronized(block)",
 	func(t *Thread, args []Value) Value {
-		lock.Lock()
-		defer lock.Unlock()
+		sy.threadLock.Lock()
+		reentry := sy.lockThread == t
+		sy.threadLock.Unlock()
+		if reentry {
+			return t.Call(args[0])
+		}
+		sy.lock.Lock()
+		sy.threadLock.Lock()
+		sy.lockThread = t
+		sy.threadLock.Unlock()
+		defer func() {
+			sy.threadLock.Lock()
+			sy.lockThread = nil
+			sy.threadLock.Unlock()
+			sy.lock.Unlock()
+		}()
 		return t.Call(args[0])
 	})
