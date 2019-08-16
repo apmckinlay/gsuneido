d3f482bc8ec8efc52389538ba83c79e6a6a082f2
Andrew McKinlay <apmckinlay@gmail.com>
2019-08-16 11:41:32 -0600

    add Exit argument default

--- builtin/exit.go
+++ builtin/exit.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/apmckinlay/gsuneido/runtime"
 )
 
-var _ = builtin1("Exit(code)",
+var _ = builtin1("Exit(code = 0)",
 	func(arg Value) Value {
 		os.Exit(IfInt(arg))
 		return nil
