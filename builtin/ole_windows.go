db2a260a0ff262e0140e440d366d200445b6e8ad
Andrew McKinlay <apmckinlay@gmail.com>
2019-08-27 08:25:02 -0600

    add OleLoadPicture

--- builtin/ole_windows.go
+++ builtin/ole_windows.go
@@ -24,3 +24,41 @@ var _ = builtin3("CreateStreamOnHGlobal(hGlobal, fDeleteOnRelease, ppstm)",
 		c.Put(nil, SuStr("x"), IntVal(int(p)))
 		return intRet(rtn)
 	})
+
+var oleaut32 = windows.NewLazyDLL("oleaut32.dll")
+
+// dll long OleAut32:OleLoadPicture(
+//		pointer lpstream,
+//		long lSize,
+//		bool fRunmode,
+//		GUID* riid,
+//		POINTER* lplpvObj)
+var oleLoadPicture = oleaut32.NewProc("OleLoadPicture")
+var _ = builtin5("OleLoadPicture(lpstream, lSize, fRunmode, riid, lplpvobj)",
+	func(a, b, c, d, e Value) Value {
+		var p uintptr
+		guid := GUID{
+			Data1: getInt32(d, "Data1"),
+			Data2: int16(getInt(d, "Data2")),
+			Data3: int16(getInt(d, "Data3")),
+		}
+		data4 := d.Get(nil, SuStr("Data4"))
+		for i := 0; i < 8; i++ {
+			guid.Data4[i] = byte(ToInt(data4.Get(nil, SuInt(i))))
+		}
+		rtn, _, _ := oleLoadPicture.Call(
+			intArg(a),
+			intArg(b),
+			boolArg(c),
+			uintptr(unsafe.Pointer(&guid)),
+			uintptr(unsafe.Pointer(&p)))
+		e.Put(nil, SuStr("x"), IntVal(int(p)))
+		return intRet(rtn)
+	})
+
+type GUID struct {
+	Data1 int32
+	Data2 int16
+	Data3 int16
+	Data4 [8]byte
+}
