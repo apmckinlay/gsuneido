35b5028addd1435069bcbf228e4b261171559e27
Andrew McKinlay <apmckinlay@gmail.com>
2019-10-09 16:33:21 -0600

    add GOARCH and GOOS to Built

--- builtin/built.go
+++ builtin/built.go
@@ -11,5 +11,6 @@ var _ = builtin0("Built()", func() Value {
 	if options.BuiltDate == "" {
 		options.BuiltDate = "Aug 19 2019 14:31:46"
 	}
-	return SuStr(options.BuiltDate + " (" + runtime.Version() + ")")
+	return SuStr(options.BuiltDate + " (" + runtime.Version() + " " +
+		runtime.GOARCH + " " + runtime.GOOS + ")")
 })
