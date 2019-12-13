4b737d6105e734b64d35469fb3820ca39fb75f90
Andrew McKinlay <apmckinlay@gmail.com>
2019-12-13 11:21:55 -0600

    fix WTS_GetSessionId

--- builtin/wtsapi_windows.go
+++ builtin/wtsapi_windows.go
@@ -28,29 +28,31 @@ const WTS_CURRENT_SESSION = uintptrMinusOne
 const WTS_ClientProtocolType = 16
 const WTS_SessionId = 4
 
-var _ = builtin0("WTS_GetClientProtocolType()",
-	func() Value {
-		defer heap.FreeTo(heap.CurSize())
-		pbuf := heap.Alloc(uintptrSize)
-		psize := heap.Alloc(int32Size)
-		rtn := goc.Syscall5(wtsQuerySessionInformation,
-			WTS_CURRENT_SERVER_HANDLE,
-			WTS_CURRENT_SESSION,
-			WTS_ClientProtocolType,
-			uintptr(pbuf),
-			uintptr(psize))
-		buf := *(*uintptr)(pbuf)
-		size := *(*int32)(psize)
-		if rtn == 0 || size != 2 || buf == 0 {
-			return Zero
-		}
-		data := *(*int16)(unsafe.Pointer(buf))
-		WTSFreeMemory(buf)
-		return IntVal(int(data))
-	})
+func WTS_GetClientProtocolType() int {
+	defer heap.FreeTo(heap.CurSize())
+	pbuf := heap.Alloc(uintptrSize)
+	psize := heap.Alloc(int32Size)
+	rtn := goc.Syscall5(wtsQuerySessionInformation,
+		WTS_CURRENT_SERVER_HANDLE,
+		WTS_CURRENT_SESSION,
+		WTS_ClientProtocolType,
+		uintptr(pbuf),
+		uintptr(psize))
+	buf := *(*uintptr)(pbuf)
+	size := *(*int32)(psize)
+	if rtn == 0 || size != 2 || buf == 0 {
+		return 0
+	}
+	data := *(*int16)(unsafe.Pointer(buf))
+	WTSFreeMemory(buf)
+	return int(data)
+}
 
 var _ = builtin0("WTS_GetSessionId()",
 	func() Value {
+		if WTS_GetClientProtocolType() == 0 {
+			return Zero
+		}
 		defer heap.FreeTo(heap.CurSize())
 		pbuf := heap.Alloc(uintptrSize)
 		psize := heap.Alloc(int32Size)
