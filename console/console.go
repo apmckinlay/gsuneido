fa73f0a5d3d5473b1991fd338e7b4f220aa4a339
Andrew McKinlay <apmckinlay@gmail.com>
2020-03-24 14:43:04 -0600

    exit if too many errors

--- console/console.go
+++ console/console.go
@@ -12,6 +12,10 @@ func LogFileAlso() {
 
 type logWriter struct{}
 
+var errCount = 0
+const errLimit = 100
+const tooMany = "too many errors, exiting"
+
 // Write outputs to Stderr and error.log
 func (lw logWriter) Write(p []byte) (n int, err error) {
 	os.Stderr.Write(p)
@@ -20,5 +24,11 @@ func (lw logWriter) Write(p []byte) (n int, err error) {
 		return 0, err
 	}
 	defer f.Close()
+	if errCount++; errCount > errLimit {
+		os.Stderr.WriteString(tooMany)
+		f.WriteString(tooMany)
+		f.Close()
+		os.Exit(-1)
+	}
 	return f.Write(p)
 }
