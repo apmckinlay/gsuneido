b25bbd5d48c78dcbfe594e82e11c2fb910796392
Andrew McKinlay <apmckinlay@gmail.com>
2020-06-01 11:26:16 -0600

    add bytes.Grow

---
+++ util/bytes/bytes.go
@@ -0,0 +1,25 @@
+// Copyright Suneido Software Corp. All rights reserved.
+// Governed by the MIT license found in the LICENSE file.
+
+package bytes
+
+const smallBufferSize = 64
+
+// Grow grows the buffer to guarantee space for n more bytes.
+func Grow(buf []byte, n int) []byte {
+	if n < 0 {
+		panic("Grow requires positive amount")
+	}
+	l := len(buf)
+	// Try to grow by reslice (fast path)
+	if n <= cap(buf)-l {
+		return buf[:l+n]
+	}
+	if buf == nil && n <= smallBufferSize {
+		return make([]byte, n, smallBufferSize)
+	}
+	// Not enough space, we need to allocate.
+	buf2 := make([]byte, 2*cap(buf) + n)
+	copy(buf2, buf)
+	return buf2[:l+n]
+}
