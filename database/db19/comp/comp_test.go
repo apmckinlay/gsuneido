86afd5d9c8b1557440f025ffbe6d083776291ecd
Andrew McKinlay <apmckinlay@gmail.com>
2020-08-18 11:13:07 -0600

    simplify comp.Key
    remove special handling for unique index

--- database/db19/comp/comp_test.go
+++ database/db19/comp/comp_test.go
@@ -13,45 +13,39 @@ import (
 )
 
 func TestKey(t *testing.T) {
-	Assert(t).That(Key(mkrec("a", "b"), []int{}, false), Is(""))
-	Assert(t).That(Key(mkrec("a", "b"), []int{0}, false), Is("a"))
-	Assert(t).That(Key(mkrec("a", "b"), []int{1}, false), Is("b"))
-	Assert(t).That(Key(mkrec("a", "b"), []int{0, 1}, false), Is("a\x00\x00b"))
-	Assert(t).That(Key(mkrec("a", "b"), []int{1, 0}, false), Is("b\x00\x00a"))
+	Assert(t).That(Key(mkrec("a", "b"), []int{}), Is(""))
+	Assert(t).That(Key(mkrec("a", "b"), []int{0}), Is("a"))
+	Assert(t).That(Key(mkrec("a", "b"), []int{1}), Is("b"))
+	Assert(t).That(Key(mkrec("a", "b"), []int{0, 1}), Is("a\x00\x00b"))
+	Assert(t).That(Key(mkrec("a", "b"), []int{1, 0}), Is("b\x00\x00a"))
 
 	// omit trailing empty fields
 	fields := []int{0, 1, 2}
-	Assert(t).That(Key(mkrec("a", "b", "c"), fields, false),
+	Assert(t).That(Key(mkrec("a", "b", "c"), fields),
 		Is("a\x00\x00b\x00\x00c"))
-	Assert(t).That(Key(mkrec("a", "", "c"), fields, false),
+	Assert(t).That(Key(mkrec("a", "", "c"), fields),
 		Is("a\x00\x00\x00\x00c"))
-	Assert(t).That(Key(mkrec("", "", "c"), fields, false),
+	Assert(t).That(Key(mkrec("", "", "c"), fields),
 		Is("\x00\x00\x00\x00c"))
-	Assert(t).That(Key(mkrec("a", "b", ""), fields, false),
+	Assert(t).That(Key(mkrec("a", "b", ""), fields),
 		Is("a\x00\x00b"))
-	Assert(t).That(Key(mkrec("a", "", ""), fields, false),
+	Assert(t).That(Key(mkrec("a", "", ""), fields),
 		Is("a"))
-	Assert(t).That(Key(mkrec("", "", ""), fields, false),
+	Assert(t).That(Key(mkrec("", "", ""), fields),
 		Is(""))
 
 	// no escape for single field
-	Assert(t).That(Key(mkrec("a\x00b"), []int{0}, false), Is("a\x00b"))
+	Assert(t).That(Key(mkrec("a\x00b"), []int{0}), Is("a\x00b"))
 
 	// escaping
-	first := []int{0,1}
-	Assert(t).That(Key(mkrec("ab"), first, false), Is("ab"))
-	Assert(t).That(Key(mkrec("a\x00b"), first, false), Is("a\x00\x01b"))
-	Assert(t).That(Key(mkrec("\x00ab"), first, false), Is("\x00\x01ab"))
-	Assert(t).That(Key(mkrec("a\x00\x00b"), first, false), Is("a\x00\x01\x00\x01b"))
-	Assert(t).That(Key(mkrec("a\x00\x01b"), first, false), Is("a\x00\x01\x01b"))
-	Assert(t).That(Key(mkrec("ab\x00"), first, false), Is("ab\x00\x01"))
-	Assert(t).That(Key(mkrec("ab\x00\x00"), first, false), Is("ab\x00\x01\x00\x01"))
-
-	// ts
-	Assert(t).That(Key(mkrec("a", "b"), []int{0, 1}, false), Is("a\x00\x00b"))
-	Assert(t).That(Key(mkrec("a", "b"), []int{0, 1}, true), Is("a"))
-	Assert(t).That(Key(mkrec("", "b"), []int{0, 1}, true), Is("\x00\x00b"))
-
+	first := []int{0, 1}
+	Assert(t).That(Key(mkrec("ab"), first), Is("ab"))
+	Assert(t).That(Key(mkrec("a\x00b"), first), Is("a\x00\x01b"))
+	Assert(t).That(Key(mkrec("\x00ab"), first), Is("\x00\x01ab"))
+	Assert(t).That(Key(mkrec("a\x00\x00b"), first), Is("a\x00\x01\x00\x01b"))
+	Assert(t).That(Key(mkrec("a\x00\x01b"), first), Is("a\x00\x01\x01b"))
+	Assert(t).That(Key(mkrec("ab\x00"), first), Is("ab\x00\x01"))
+	Assert(t).That(Key(mkrec("ab\x00\x00"), first), Is("ab\x00\x01\x00\x01"))
 }
 
 func mkrec(args ...string) Record {
@@ -73,8 +67,8 @@ func TestRandom(t *testing.T) {
 	for i := 0; i < n; i++ {
 		x := gen()
 		y := gen()
-		yenc := Key(y, fields, false)
-		xenc := Key(x, fields, false)
+		yenc := Key(y, fields)
+		xenc := Key(x, fields)
 		Assert(t).That(xenc < yenc, Is(lt(x, y)))
 		Assert(t).That(strings.Compare(xenc, yenc), Is(Compare(x, y, fields)))
 	}
--- database/db19/comp/composite.go
+++ database/db19/comp/composite.go
@@ -12,14 +12,10 @@ import (
 
 	. "github.com/apmckinlay/gsuneido/runtime"
 	"github.com/apmckinlay/gsuneido/util/hacks"
-	"github.com/apmckinlay/gsuneido/util/verify"
 )
 
 // Key builds a composite key string that is comparable raw.
-// If ui (unique index) is true
-// then the final field will only be added if the other fields are all empty.
-func Key(rec Record, fields []int, ui bool) string {
-	verify.That(ui == false || len(fields) >= 2)
+func Key(rec Record, fields []int) string {
 	if len(fields) == 0 {
 		return ""
 	}
@@ -30,9 +26,6 @@ func Key(rec Record, fields []int, ui bool) string {
 	n := 0
 	lastNonEmpty := 0
 	for i, field := range fields {
-		if ui && i == len(fields)-1 && n > 0 {
-			break
-		}
 		fldlen := len(rec.GetRaw(field))
 		if fldlen > 0 {
 			lastNonEmpty = i
