ce07cbb6f2265e13c250f886025921964206f74b
Andrew McKinlay <apmckinlay@gmail.com>
2020-09-21 11:04:34 -0600

    add buffering to File io

--- builtin/file.go
+++ builtin/file.go
@@ -4,6 +4,7 @@
 package builtin
 
 import (
+	"bufio"
 	"io"
 	"os"
 	"strings"
@@ -17,6 +18,12 @@ type suFile struct {
 	name string
 	mode string
 	f    *os.File
+	r    *bufio.Reader // only one of r or w will be used
+	w    *bufio.Writer
+	// tell is used to track our own position in the file.
+	// We can't use f.Tell() because of buffering.
+	// Any reads or writes must update this.
+	tell int64
 }
 
 var nFile = 0
@@ -25,12 +32,7 @@ var _ = builtin("File(filename, mode='r', block=false)",
 	func(t *Thread, args []Value) Value {
 		name := ToStr(args[0])
 		mode := ToStr(args[1])
-
-		f, err := os.OpenFile(name, modeToFlags(mode), 0644)
-		if err != nil {
-			panic("File: can't " + err.Error())
-		}
-		sf := &suFile{name: name, mode: mode, f: f}
+		sf := newSuFile(name, mode)
 		nFile++
 		if args[2] == False {
 			return sf
@@ -40,21 +42,54 @@ var _ = builtin("File(filename, mode='r', block=false)",
 		return t.Call(args[2], sf)
 	})
 
-func modeToFlags(mode string) int {
+func newSuFile(name, mode string) *suFile {
+	var perm int
 	switch mode {
 	case "r":
-		return os.O_RDONLY
+		perm = os.O_RDONLY
 	case "a":
-		return os.O_RDWR | os.O_CREATE | os.O_APPEND
+		perm = os.O_WRONLY | os.O_CREATE | os.O_APPEND
 	case "w":
-		return os.O_RDWR | os.O_CREATE | os.O_TRUNC
+		perm = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
 	default:
 		panic("File: invalid mode")
 	}
+	f, err := os.OpenFile(name, perm, 0644)
+	if err != nil {
+		panic("File: can't " + err.Error())
+	}
+	sf := &suFile{name: name, mode: mode, f: f}
+	if sf.mode == "r" {
+		sf.r = bufio.NewReader(f)
+	} else { // "w" or "a"
+		sf.w = bufio.NewWriter(f)
+	}
+	if sf.mode == "a" {
+		sf.tell = sf.size()
+	}
+	return sf
+}
+
+// reset is called by Seek to reset buffering
+func (sf *suFile) reset() {
+	if sf.mode == "r" {
+		sf.r.Reset(sf.f)
+	} else {
+		sf.w.Flush()
+		sf.w.Reset(sf.f)
+	}
+}
+
+func (sf *suFile) size() int64 {
+	info, _ := sf.f.Stat()
+	return info.Size()
 }
 
 func (sf *suFile) close() {
 	nFile--
+	if sf.mode != "r" {
+		sf.w.Flush()
+	}
 	sf.f.Close()
 	sf.f = nil
 }
@@ -118,16 +153,13 @@ var suFileMethods = Methods{
 		return nil
 	}),
 	"Flush": method0(func(this Value) Value {
-		// no buffering so nothing to do
-		sfOpen(this).f.Sync()
+		sfOpenWrite(this).w.Flush()
 		return nil
 	}),
 	"Read": method1("(nbytes=false)", func(this, arg Value) Value {
-		f := sfOpen(this).f
-		pos, _ := f.Seek(0, io.SeekCurrent)
-		info, _ := f.Stat()
-		n := int(info.Size() - pos)
-		if n == 0 {
+		sf := sfOpenRead(this)
+		n := int(sf.size() - sf.tell) // remaining
+		if n == 0 {                   // at end
 			return False
 		}
 		if arg != False {
@@ -136,49 +168,69 @@ var suFileMethods = Methods{
 			}
 		}
 		buf := make([]byte, n)
-		_, err := io.ReadFull(f, buf)
+		_, err := io.ReadFull(sf.r, buf)
+		sf.tell += int64(n)
 		if err != nil {
 			panic("file.Read " + err.Error())
 		}
 		return SuStr(string(buf))
 	}),
 	"Readline": method0(func(this Value) Value {
-		return Readline(sfOpen(this).f, "file.Readline: ")
+		sf := sfOpenRead(this)
+		val, nr := readline(sf.r, "file.Readline: ")
+		sf.tell += int64(nr)
+		return val
 	}),
 	"Seek": method2("(offset, origin='set')", func(this, arg1, arg2 Value) Value {
-		offset := ToInt64(arg1)
-		if offset < 0 {
-			offset = 0
+		sf := sfOpen(this)
+		if sf.mode == "a" {
+			panic("file.Seek: invalid with mode 'a'")
 		}
-		var whence int
+		offset := ToInt64(arg1)
 		switch ToStr(arg2) {
-		case "cur":
-			whence = io.SeekCurrent
 		case "set":
-			whence = io.SeekStart
+			//
+		case "cur":
+			offset += sf.tell
 		case "end":
-			whence = io.SeekEnd
+			offset += sf.size()
 		default:
 			panic("file.Seek: origin must be 'set', 'end', or 'cur'")
 		}
-		_, err := sfOpen(this).f.Seek(offset, whence)
+		_, err := sf.f.Seek(offset, io.SeekStart)
 		if err != nil {
-			panic("file.Seek " + err.Error())
+			panic("file.Seek: " + err.Error())
 		}
+		sf.tell = offset
+		sf.reset()
 		return nil
 	}),
 	"Tell": method0(func(this Value) Value {
-		pos, _ := sfOpen(this).f.Seek(0, io.SeekCurrent)
-		return Int64Val(pos)
+		sf := sfOpen(this)
+		if sf.mode == "a" {
+			panic("file.Tell: invalid with mode 'a'")
+		}
+		return Int64Val(sf.tell)
 	}),
 	"Write": method1("(string)", func(this, arg Value) Value {
-		sfOpen(this).f.WriteString(AsStr(arg))
+		s := AsStr(arg)
+		sf := sfOpenWrite(this)
+		_, err := sf.w.WriteString(s)
+		if err != nil {
+			panic("File: Write: " + err.Error())
+		}
+		sf.tell += int64(len(s))
 		return arg
 	}),
 	"Writeline": method1("(string)", func(this, arg Value) Value {
-		f := sfOpen(this).f
-		f.WriteString(AsStr(arg))
-		f.WriteString("\n")
+		s := AsStr(arg)
+		sf := sfOpen(this)
+		sf.w.WriteString(s)
+		_, err := sf.w.WriteString("\n")
+		if err != nil {
+			panic("File: Writeline: " + err.Error())
+		}
+		sf.tell += int64(len(s) + 1)
 		return arg
 	}),
 }
@@ -191,20 +243,43 @@ func sfOpen(this Value) *suFile {
 	return sf
 }
 
+func sfOpenRead(this Value) *suFile {
+	sf := sfOpen(this)
+	if sf.mode != "r" {
+		panic("File: can't read a file opened for writing")
+	}
+	return sf
+}
+
+func sfOpenWrite(this Value) *suFile {
+	sf := sfOpen(this)
+	if sf.mode == "r" {
+		panic("File: can't write a file opened for reading")
+	}
+	return sf
+}
+
 func Readline(rdr io.Reader, errPrefix string) Value {
+	val, _ := readline(rdr, errPrefix)
+	return val
+}
+
+func readline(rdr io.Reader, errPrefix string) (Value, int) {
+	nr := 0
 	var buf strings.Builder
 	b := make([]byte, 1)
 	for {
-		n, err := rdr.Read(b)
-		if n == 0 {
+		_, err := rdr.Read(b)
+		if err == io.EOF {
 			if buf.Len() == 0 {
-				return False
+				return False, nr
 			}
 			break
 		}
 		if err != nil {
 			panic(errPrefix + err.Error())
 		}
+		nr++
 		if b[0] == '\n' {
 			break
 		}
@@ -214,5 +289,5 @@ func Readline(rdr io.Reader, errPrefix string) Value {
 	}
 	s := buf.String()
 	s = strings.TrimRight(s, "\r")
-	return SuStr(s)
+	return SuStr(s), nr
 }
