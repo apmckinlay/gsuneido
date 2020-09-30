8e11b54c8fd2410179cdebd81ccc77d1048c39d0
Andrew McKinlay <apmckinlay@gmail.com>
2020-09-30 16:36:40 -0600

    fix Date(...) to be consistent with c/jSuneido

--- builtin/date.go
+++ builtin/date.go
@@ -17,8 +17,8 @@ type SuDateGlobal struct {
 
 func init() {
 	name, ps := paramSplit(`Date(string=false, pattern=false,
-		year=false, month=false, day=false,
-		hour=false, minute=false, second=false, millisecond=false)`)
+		year=nil, month=nil, day=nil,
+		hour=nil, minute=nil, second=nil, millisecond=nil)`)
 	Global.Builtin(name, &SuDateGlobal{SuBuiltin{Fn: dateCallClass,
 		BuiltinParams: BuiltinParams{ParamSpec: *ps}}})
 }
@@ -55,7 +55,7 @@ func dateCallClass(_ *Thread, args []Value) Value {
 
 func hasFields(args []Value) bool {
 	for i := 2; i <= 8; i++ {
-		if args[i] != False {
+		if args[i] != nil {
 			return true
 		}
 	}
@@ -71,25 +71,25 @@ func named(args []Value) Value {
 	minute := now.Minute()
 	second := now.Second()
 	millisecond := now.Millisecond()
-	if args[2] != False {
+	if args[2] != nil {
 		year = ToInt(args[2])
 	}
-	if args[3] != False {
+	if args[3] != nil {
 		month = ToInt(args[3])
 	}
-	if args[4] != False {
+	if args[4] != nil {
 		day = ToInt(args[4])
 	}
-	if args[5] != False {
+	if args[5] != nil {
 		hour = ToInt(args[5])
 	}
-	if args[6] != False {
+	if args[6] != nil {
 		minute = ToInt(args[6])
 	}
-	if args[7] != False {
+	if args[7] != nil {
 		second = ToInt(args[7])
 	}
-	if args[8] != False {
+	if args[8] != nil {
 		millisecond = ToInt(args[8])
 	}
 	return NormalizeDate(year, month, day, hour, minute, second, millisecond)
