62320f597986e8e199d5fa2fc526ae002b50f0d9
Andrew McKinlay <apmckinlay@gmail.com>
2021-07-21 12:43:46 -0600

    fix concur_test.go
    for Windows low timer resolution
    and names reversed

--- runtime/concur_test.go
+++ runtime/concur_test.go
@@ -15,7 +15,7 @@ import (
 
 // NOTE: these tests depend on the race detector to find problems
 
-func TestConcurrentAtomic(t *testing.T) {
+func TestConcurrentMutex(t *testing.T) {
 	if !race.Enabled {
 		t.Skip("RACE NOT ENABLED")
 	}
@@ -23,19 +23,19 @@ func TestConcurrentAtomic(t *testing.T) {
 	var concurrent bool
 	var mu sync.Mutex
 	go func() {
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(40 * time.Millisecond)
 		mu.Lock()
 		mu.Unlock()
 		assert.That(concurrent == true)
 	}()
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(20 * time.Millisecond)
 	mu.Lock()
 	concurrent = true
 	mu.Unlock()
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(20 * time.Millisecond)
 }
 
-func TestConcurrentMutex(t *testing.T) {
+func TestConcurrentAtomic(t *testing.T) {
 	if !race.Enabled {
 		t.Skip("RACE NOT ENABLED")
 	}
@@ -43,14 +43,14 @@ func TestConcurrentMutex(t *testing.T) {
 	var x int
 	var m int64
 	go func() {
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(40 * time.Millisecond)
 		assert.That(atomic.LoadInt64(&m) == 1)
 		assert.That(x == 123)
 	}()
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(20 * time.Millisecond)
 	x = 123
 	atomic.StoreInt64(&m, 1)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(20 * time.Millisecond)
 }
 
 func TestConcurrentSuObjectIter(t *testing.T) {
@@ -61,7 +61,7 @@ func TestConcurrentSuObjectIter(t *testing.T) {
 	ob.SetConcurrent()
 	for i := 0; i < 4; i++ {
 		go func() {
-			time.Sleep(5 * time.Millisecond)
+			time.Sleep(20 * time.Millisecond)
 			iter := ob.Iter2(true, true)
 			iter()
 		}()
