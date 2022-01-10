374f8d14e2dc5d79006d16b4cc240cd6cc2766af
Andrew McKinlay <apmckinlay@gmail.com>
2022-01-10 11:06:45 -0800

    fix strs.Join length calculation

--- util/strs/strs.go
+++ util/strs/strs.go
@@ -111,9 +111,11 @@ func Join(fmt string, list []string) string {
 		suffix = fmt[nf-1:]
 		fmt = fmt[1 : nf-1]
 	}
-	n := len(fmt) * (nf - 1)
+	n := len(prefix) + len(suffix)
+	sn := 0
 	for _, s := range list {
-		n += len(s)
+		n += sn + len(s)
+		sn = len(fmt)
 	}
 	sep := ""
 	var sb strings.Builder
