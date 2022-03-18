cb7341070dd62713120fb2e4a356bec17800bd16
Andrew McKinlay <apmckinlay@gmail.com>
2022-03-18 12:05:14 -0700

    don't redirect if already redirected

--- util/system/redir_windows.go
+++ util/system/redir_windows.go
@@ -10,9 +10,13 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// Redirect redirects stderr and stdout to a file.
+// Redirect redirects stderr and stdout to a file
+// (unless already redirected).
 // This is to capture Go errors when running as a GUI program or service.
 func Redirect(filename string) error {
+	if redirected() {
+		return nil
+	}
 	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return err
@@ -30,4 +34,14 @@ func Redirect(filename string) error {
 	os.Stderr = f
 	os.Stdout = f
 	return nil
+}
+
+func redirected() bool {
+	handle, _ := windows.GetStdHandle(windows.STD_OUTPUT_HANDLE)
+	if handle != 0 {
+		dwFileType, _ := windows.GetFileType(handle)
+		return dwFileType == windows.FILE_TYPE_DISK ||
+			dwFileType == windows.FILE_TYPE_PIPE
+	}
+	return false
 }
