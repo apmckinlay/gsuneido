6073dae3f24ef9271ea1ffc27b4d446cb9494aaf
Andrew McKinlay <apmckinlay@gmail.com>
2022-08-12 16:37:34 -0700

    remove Fatal PrintStack
    not helpful in most cases

--- runtime/fatal.go
+++ runtime/fatal.go
@@ -7,16 +7,15 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/apmckinlay/gsuneido/util/dbg"
 	"github.com/apmckinlay/gsuneido/util/exit"
 )
 
 func Fatal(args ...any) {
 	s := fmt.Sprintln(args...)
 	log.Print("FATAL: ", s)
-	if args[0] != "lost connection" && args[0] != "Can't connect." {
-		dbg.PrintStack()
-	}
+	// if args[0] != "lost connection" && args[0] != "Can't connect." {
+	// 	dbg.PrintStack()
+	// }
 	Fatal2(s)
 	exit.Exit(1)
 }
