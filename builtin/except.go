b532497742609690bc6166a359c3ab7aef477b4d
Andrew McKinlay <apmckinlay@gmail.com>
2022-09-18 08:42:24 -0700

    add except.As
    from Hao

--- builtin/except.go
+++ builtin/except.go
@@ -14,3 +14,10 @@ var _ = method(except_Callstack, "()")
 func except_Callstack(this Value) Value {
 	return this.(*SuExcept).Callstack
 }
+
+var _ = method(except_As, "(string)")
+
+func except_As(this Value, val Value) Value {
+	return &SuExcept{SuStr: SuStr(AsStr(val)),
+		Callstack: this.(*SuExcept).Callstack}
+}
