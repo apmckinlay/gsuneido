acc3c5540f060e5e5e06e8c346fbf54191cdbc30
Andrew McKinlay <apmckinlay@gmail.com>
2023-01-24 16:22:59 -0800

    test with libraries e.g. for profiling

---
+++ full_test.go
@@ -0,0 +1,39 @@
+// Governed by the MIT license found in the LICENSE file.
+
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/apmckinlay/gsuneido/builtin"
+// 	"github.com/apmckinlay/gsuneido/options"
+// 	. "github.com/apmckinlay/gsuneido/runtime"
+// )
+
+// func TestFull(t *testing.T) {
+// 	options.BuiltDate = builtDate
+// 	Libload = libload // dependency injection
+// 	mainThread = &Thread{}
+// 	mainThread.Name = "main"
+// 	mainThread.UIThread = true
+// 	MainThread = mainThread
+// 	builtin.UIThread = mainThread
+// 	openDbms()
+// 	defer db.CloseKeepMapped()
+// 	run("Init.Repl()")
+// 	run(`Use("axonlib")
+// 		Use("Accountinglib")
+// 		Use("etalib")
+// 		Use("pcmiler")
+// 		Use("ticketlib")
+// 		Use("prlib")
+// 		Use("prcadlib")
+// 		Use("etaprlib")
+// 		Use("invenlib")
+// 		Use("wolib")
+// 		Use("polib")
+// 		Use("configlib")
+// 		Use("demobookoptions")
+// 		Use("Test_lib")
+// 		TestRunner.Run(libs: #(etalib))`)
+// }
