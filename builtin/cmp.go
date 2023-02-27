c65a33dba6eaab89906de4bb37f1df548fc7dbf9
Andrew McKinlay <apmckinlay@gmail.com>
2023-02-27 14:07:34 -0800

    add builtin Cmp function

---
+++ builtin/cmp.go
@@ -0,0 +1,14 @@
+// Copyright Suneido Software Corp. All rights reserved.
+// Governed by the MIT license found in the LICENSE file.
+
+package builtin
+
+import (
+	. "github.com/apmckinlay/gsuneido/runtime"
+)
+
+var _ = builtin(Cmp, "(x, y)")
+
+func Cmp(x, y Value) Value {
+	return IntVal(x.Compare(y))
+}
