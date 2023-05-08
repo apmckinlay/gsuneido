3db93af403aa7f014e429968897a8627c072b4da
Adhyan H <adhyanpatel@gmail.com>
2023-05-08 09:36:46 -0600

    reverted repl changes

--- gsuneido.go
+++ gsuneido.go
@@ -352,10 +352,6 @@ func repl() {
 	showOptions()
 	prompt("Press Enter twice (i.e. blank line) to execute, q to quit")
 	r := bufio.NewReader(os.Stdin)
-
-	// array of string to hold repl history
-	repl_history := []string{}
-
 	for {
 		prompt("~~~")
 		src := ""
@@ -370,7 +366,6 @@ func repl() {
 			}
 			src += line + "\n"
 		}
-		repl_history = append(repl_history, src)
 		eval(src)
 	}
 }
