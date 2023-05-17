13b56a32119a058f237a4fe0cd1f9501527892ff
Andrew McKinlay <apmckinlay@gmail.com>
2023-05-17 10:38:09 -0700

    db query: fix TempIndex Fixed
    broken by recent caching changes

--- dbms/query/tempindex.go
+++ dbms/query/tempindex.go
@@ -41,6 +41,7 @@ func NewTempIndex(src Query, order []string, tran QueryTran) *TempIndex {
 	ti.source = src
 	ti.header = src.Header().Dup() // dup because sortlist is concurrent
 	ti.keys = src.Keys()
+	ti.fixed = src.Fixed()
 	ti.nNrows, ti.pNrows = src.Nrows()
 	ti.rowSiz = src.rowSize()
 	return &ti
