1f253197bfa2c6344a3b7eb2b69c8898cd0567c1
Andrew McKinlay <apmckinlay@gmail.com>
2024-02-06 15:10:59 -0800

    add asymmetric keygen, encrypt, decrypt

--- builtin/openpgp.go
+++ builtin/openpgp.go
@@ -13,8 +13,9 @@ import (
 	. "github.com/apmckinlay/gsuneido/core"
 	"github.com/apmckinlay/gsuneido/util/system"
 
-	//lint:ignore SA1019 best option
-	"golang.org/x/crypto/openpgp"
+	"github.com/ProtonMail/go-crypto/openpgp"
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+	"github.com/ProtonMail/gopenpgp/v2/helper"
 	"golang.org/x/exp/maps"
 )
 
@@ -51,9 +52,9 @@ var _ = staticMethod(opgp_SymmetricEncrypt,
 
 func opgp_SymmetricEncrypt(passphrase, source, toFile Value) Value {
 	if toFile == False {
-		return symStr(passphrase, source, symEncrypt)
+		return opgpStr(passphrase, source, symEncrypt)
 	}
-	return symFile(passphrase, source, toFile, symEncrypt)
+	return opgpFile(passphrase, source, toFile, symEncrypt)
 }
 
 var _ = staticMethod(opgp_SymmetricDecrypt,
@@ -61,21 +62,69 @@ var _ = staticMethod(opgp_SymmetricDecrypt,
 
 func opgp_SymmetricDecrypt(passphrase, source, toFile Value) Value {
 	if toFile == False {
-		return symStr(passphrase, source, symDecrypt)
+		return opgpStr(passphrase, source, symDecrypt)
 	}
-	return symFile(passphrase, source, toFile, symDecrypt)
+	return opgpFile(passphrase, source, toFile, symDecrypt)
 }
 
-type encdec func(passphrase string, src io.Reader, dst io.Writer)
+var _ = staticMethod(opgp_PublicEncrypt,
+	"(publicKey, source, toFile = false)")
 
-func symStr(passphrase Value, source Value, f encdec) Value {
+func opgp_PublicEncrypt(publicKey, source, toFile Value) Value {
+	if toFile == False {
+		return opgpStr(ToStr(publicKey), source, asymEncrypt)
+	}
+	return opgpFile(ToStr(publicKey), source, toFile, asymEncrypt)
+}
+
+var _ = staticMethod(opgp_PrivateDecrypt,
+	"(privateKey, passphrase, source, toFile = false)")
+
+func opgp_PrivateDecrypt(privateKey, passphrase, source, toFile Value) Value {
+	kp := keyPair{privateKey: ToStr(privateKey), passphrase: ToStr(passphrase)}
+	if toFile == False {
+		return opgpStr(kp, source, asymDecrypt)
+	}
+	return opgpFile(kp, source, toFile, asymDecrypt)
+}
+
+type keyPair struct {
+	privateKey string
+	passphrase string
+}
+
+var _ = staticMethod(opgp_KeyGen, "(name, email, passphrase)")
+
+func opgp_KeyGen(name, email, passphrase Value) Value {
+	const rsaBits = 2048
+	privateKey, err := helper.GenerateKey(ToStr(name), ToStr(email),
+		[]byte(ToStr(passphrase)), "rsa", rsaBits)
+	ck(err)
+
+	keyRing, err := crypto.NewKeyFromArmoredReader(strings.NewReader(privateKey))
+	ck(err)
+
+	publicKey, err := keyRing.GetArmoredPublicKey()
+	ck(err)
+
+	ob := &SuObject{}
+	ob.Set(SuStr("public"), SuStr(publicKey))
+	ob.Set(SuStr("private"), SuStr(privateKey))
+	return ob
+}
+
+//-------------------------------------------------------------------
+
+type encdec[T any] func(passphrase T, src io.Reader, dst io.Writer)
+
+func opgpStr[T any](key T, source Value, f encdec[T]) Value {
 	src := strings.NewReader(ToStr(source))
 	dst := new(bytes.Buffer)
-	f(ToStr(passphrase), src, dst)
+	f(key, src, dst)
 	return SuStr(dst.String())
 }
 
-func symFile(passphrase Value, fromFile, toFile Value, f encdec) Value {
+func opgpFile[T any](key T, fromFile, toFile Value, f encdec[T]) Value {
 	src, err := os.Open(ToStr(fromFile))
 	ck(err)
 	defer src.Close()
@@ -84,27 +133,54 @@ func symFile(passphrase Value, fromFile, toFile Value, f encdec) Value {
 	ck(err)
 	defer os.Remove(dst.Name())
 	defer dst.Close()
-	f(ToStr(passphrase), src, dst)
+	f(key, src, dst)
 	dst.Close()
 	system.RenameBak(dst.Name(), to)
 	return nil
 }
 
-func symEncrypt(passphrase string, src io.Reader, dst io.Writer) {
-	encrypter, err := openpgp.SymmetricallyEncrypt(dst, []byte(passphrase), nil, nil)
+func symEncrypt(passphrase Value, src io.Reader, dst io.Writer) {
+	encrypter, err := openpgp.SymmetricallyEncrypt(
+		dst, []byte(ToStr(passphrase)), nil, nil)
 	ck(err)
 	defer encrypter.Close()
 	_, err = io.Copy(encrypter, src)
 	ck(err)
 }
 
-func symDecrypt(passphrase string, src io.Reader, dst io.Writer) {
+func symDecrypt(passphrase Value, src io.Reader, dst io.Writer) {
 	keyRing := func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
-		return []byte(passphrase), nil
+		return []byte(ToStr(passphrase)), nil
 	}
 	md, err := openpgp.ReadMessage(src, nil, keyRing, nil)
 	ck(err)
 	_, err = io.Copy(dst, md.UnverifiedBody)
+	ck(err)
+}
+
+func asymEncrypt(publicKey string, src io.Reader, dst io.Writer) {
+	publicKeyObj, err := crypto.NewKeyFromArmored(publicKey)
+	ck(err)
+	publicKeyRing, err := crypto.NewKeyRing(publicKeyObj)
+	ck(err)
+	encryptor, err := publicKeyRing.EncryptStreamWithCompression(dst, nil, nil)
+	ck(err)
+	defer encryptor.Close()
+	_, err = io.Copy(encryptor, src)
+	ck(err)
+}
+
+func asymDecrypt(kp keyPair, src io.Reader, dst io.Writer) {
+	privateKeyObj, err := crypto.NewKeyFromArmored(kp.privateKey)
+	ck(err)
+	privateKeyUnlocked, err := privateKeyObj.Unlock([]byte(kp.passphrase))
+	ck(err)
+	defer privateKeyUnlocked.ClearPrivateParams()
+	privateKeyRing, err := crypto.NewKeyRing(privateKeyUnlocked)
+	ck(err)
+	decryptor, err := privateKeyRing.DecryptStream(src, nil, 0)
+	ck(err)
+	_, err = io.Copy(dst, decryptor)
 	ck(err)
 }
 
--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module github.com/apmckinlay/gsuneido
 go 1.21
 
 require (
-	github.com/google/uuid v1.5.0
+	github.com/ProtonMail/go-crypto v1.1.0-alpha.0-proton
+	github.com/ProtonMail/gopenpgp/v2 v2.7.4
+	github.com/google/uuid v1.6.0
 	github.com/kljensen/snowball v0.9.0
-	golang.org/x/crypto v0.17.0
-	golang.org/x/exp v0.0.0-20231219180239-dc181d75b848
-	golang.org/x/sys v0.15.0
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a
+	golang.org/x/sys v0.16.0
 	golang.org/x/text v0.14.0
 	golang.org/x/time v0.5.0
+)
+
+require (
+	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
 )
