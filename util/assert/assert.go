c107a49a087cc47c12fe171c1e8b074ed578786f
Andrew McKinlay <apmckinlay@gmail.com>
2024-08-14 16:51:46 -0700

    comment

--- util/assert/assert.go
+++ util/assert/assert.go
@@ -374,9 +374,10 @@ func (a assert) fail(args ...any) {
 	}
 }
 
+// ShouldNotReachHere logs and panics.
+// The return type is so it can be called like panic(ShouldNotReachHere())
+// so the compiler knows it doesn't continue
 func ShouldNotReachHere() int {
-	// return type is so it can be called like panic(ShouldNotReachHere())
-	// so the compiler knows it doesn't continue
 	e := "ASSERT FAILED: should not reach here"
 	log.Println(e)
 	dbg.PrintStack()
