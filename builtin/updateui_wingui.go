a7deca29bd2c2d4bb98df1f6f05fffad1d675b77
Andrew McKinlay <apmckinlay@gmail.com>
2024-10-28 08:59:06 -0700

    remove UpdateUI - no longer used

--- builtin/updateui_wingui.go
+++ builtin/updateui_wingui.go
@@ -6,48 +6,12 @@
 package builtin
 
 import (
-	"log"
-
 	. "github.com/apmckinlay/gsuneido/core"
-	"github.com/apmckinlay/gsuneido/util/dbg"
 )
 
 // rogsChan is used by other threads to Run code On the Go Side UI thread
 // Need buffer so we can send to channel and then notifyCside
 var rogsChan = make(chan func(), 1)
-
-// UpdateUI runs the block on the main UI thread
-var _ = builtin(UpdateUI, "(block)")
-
-// UpdateUI runs the block on the main UI thread
-// The block will be run in one of two ways:
-// If executing in the interpreter in MainThread,
-// it periodically calls runOnGoSide.
-// If executing in the C message loop,
-// the cside timer will trigger a call back to runOnGoSide.
-func UpdateUI(th *Thread, args []Value) Value {
-	block := args[0]
-	if th == MainThread {
-		th.Call(block)
-	} else {
-		block.SetConcurrent()
-		rogsChan <- func() { runUI(block) }
-	}
-	return nil
-}
-
-func runUI(block Value) {
-	state := MainThread.GetState()
-	defer func() {
-		if e := recover(); e != nil {
-			log.Println("ERROR: in UpdateUI:", e)
-			MainThread.PrintStack()
-			dbg.PrintStack()
-		}
-		MainThread.RestoreState(state)
-	}()
-	MainThread.Call(block)
-}
 
 // runOnGoSide is called by interp via runtime.RunOnGoSide
 // and cside via goc.RunOnGoSide
