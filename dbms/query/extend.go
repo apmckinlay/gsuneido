3ecd396a0ef7a78c673534b21891da7ce71318c8
Andrew McKinlay <apmckinlay@gmail.com>
2025-03-14 17:34:44 -0700

    extend - minor refactor

--- dbms/query/extend.go
+++ dbms/query/extend.go
@@ -275,12 +275,18 @@ func (e *Extend) extendRow(th *Thread, row Row) Record {
 }
 
 func (e *Extend) filter(rec Record, th *Thread, row Row) bool {
+	var extrow Row
 	for i, col := range e.selCols {
 		j := slices.Index(e.physical, col)
-		x := rec.GetRaw(j)
-		if j < 0 || (len(x) > 0 && x[0] == PackForward) {
-			row = append(row, DbRec{Record: rec})
-			x = row.GetRawVal(e.header, col, th, e.ctx.Tran)
+		var x string
+		if j >= 0 {
+			x = rec.GetRaw(j)
+		}
+		if j == -1 || (len(x) > 0 && x[0] == PackForward) {
+			if extrow == nil {
+				extrow = append(row, DbRec{Record: rec})
+			}
+			x = extrow.GetRawVal(e.header, col, th, e.ctx.Tran)
 		}
 		if x != e.selVals[i] {
 			return false
