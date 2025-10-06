ad1732000e7df7f2b1745bed0fe773a99e59e56a
Andrew McKinlay <apmckinlay@gmail.com>
2025-10-06 11:54:42 -0700

    add util/bits/Shuffle16 and 32

--- util/bits/bits.go
+++ util/bits/bits.go
@@ -15,3 +15,27 @@ func NextPow2(n uint) int {
 func TrailingOnes(x int) int {
 	return bits.TrailingZeros(^uint(x))
 }
+
+func Shuffle32(n uint32) uint32 {
+	// Add a large, odd constant to break the fixed point at 0.
+	// 0x9e3779b9 is derived from the golden ratio and is a common choice.
+	n += 0x9e3779b9
+	n ^= n >> 16
+	n *= 0x85ebca6b
+	n ^= n >> 13
+	n *= 0xc2b2ae35
+	n ^= n >> 16
+	return n
+}
+
+func Shuffle16(n uint16) uint16 {
+	// Add a constant to the initial state to move it away from 0.
+	// This can be any number, but a large one is good practice.
+	n += 0xda79
+	n ^= n >> 7
+	n *= 0x6955
+	n ^= n >> 9
+	n *= 0xde59
+	n ^= n >> 8
+	return n
+}
