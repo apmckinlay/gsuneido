5e10b37b52d744c103aea0e446ba5f4296a010ae
Andrew McKinlay <apmckinlay@gmail.com>
2025-10-07 17:09:52 -0700

    add test for bits.Shuffle

--- util/bits/bits_test.go
+++ util/bits/bits_test.go
@@ -4,6 +4,7 @@
 package bits
 
 import (
+	"math"
 	"testing"
 
 	"github.com/apmckinlay/gsuneido/util/assert"
@@ -17,4 +18,31 @@ func TestNextPow2(t *testing.T) {
 	assert(NextPow2(3)).Is(4)
 	assert(NextPow2(123)).Is(128)
 	assert(NextPow2(65536)).Is(65536)
+}
+
+func TestShuffle16(t *testing.T) {
+	assert := assert.T(t).This
+	sum1 := 0
+	sum2 := 0
+	for i := 0; i <= math.MaxUint16; i++ {
+		sum1 += i
+		sum2 += int(Shuffle16(uint16(i)))
+	}
+	assert(sum1).Is((math.MaxUint16 * (math.MaxUint16 + 1)) / 2)
+	assert(sum1).Is(sum2)
+}
+
+func TestShuffle32(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping long test")
+	}
+	assert := assert.T(t).This
+	sum1 := 0
+	sum2 := 0
+	for i := 0; i <= math.MaxUint32; i++ {
+		sum1 += i
+		sum2 += int(Shuffle32(uint32(i)))
+	}
+	assert(sum1).Is((math.MaxUint32 * (math.MaxUint32 + 1)) / 2)
+	assert(sum1).Is(sum2)
 }
