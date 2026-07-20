02a2d0f34e18702888270fb610e384c4e5f5bd80
Andrew McKinlay <apmckinlay@gmail.com>
2026-07-19 17:14:27 -0700

    query - move String and Strategy to format.go

--- dbms/query/format.go
+++ dbms/query/format.go
@@ -4,8 +4,12 @@
 package query
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/apmckinlay/gsuneido/core/trace"
+	"github.com/apmckinlay/gsuneido/util/str"
 )
 
 func Format(t QueryTran, query string) string {
@@ -61,4 +65,109 @@ func addParens(s string) string {
 		i++
 	}
 	return s[:i] + "(" + s[i:] + ")"
+}
+
+//-------------------------------------------------------------------
+
+// String prints the full query, including child sources
+// whereas query.String only shows that operation
+func String(q Query) string {
+	switch qi := q.(type) {
+	case q2i:
+		return paren2(qi.Source()) + " " + q.String() + " " + paren1(qi.Source2())
+	case *Sort:
+		return String(qi.Source()) + str.Opt(" ", q.String()) // no parens
+	case *View:
+		return q.String()
+	case q1i:
+		return paren2(qi.Source()) + str.Opt(" ", q.String())
+	default:
+		return q.String()
+	}
+}
+
+func paren1(q Query) string {
+	switch q.(type) {
+	case *Table, *Tables, *TablesLookup, *Columns, *Indexes, *Views,
+		*Nothing, *ProjectNone:
+		return String(q)
+	}
+	return "(" + String(q) + ")"
+}
+
+func paren2(q Query) string {
+	if _, ok := q.(q2i); ok {
+		return "(" + String(q) + ")"
+	}
+	return String(q)
+}
+
+//-------------------------------------------------------------------
+
+func Strategy(q Query) string {
+	return strategy(q, 0)
+}
+
+const indent1 = "    "
+
+func strategy(q Query, indent int) string { // recursive
+	in := strings.Repeat(indent1, indent)
+	nrows, pop := q.Nrows()
+	m := q.Metrics()
+	cost := "{"
+	if m.frac != 1 {
+		cost += fmt.Sprintf("%.3fx ", m.frac)
+	}
+	cost += trace.Number(nrows)
+	if nrows != pop {
+		cost += "/" + trace.Number(pop)
+	}
+	cost += " " + trace.Number(m.fixcost) + "+" + trace.Number(m.varcost)
+	cost += "} "
+	switch q := q.(type) {
+	case *Sort:
+		if q.String() == "" {
+			return strategy(q.Source(), indent)
+		} else {
+			return strategy(q.Source(), indent) + "\n" +
+				in + cost + q.String()
+		}
+	case q2i:
+		return strategy(q.Source(), indent+1) + "\n" +
+			in + cost + q.String() + "\n" +
+			strategy(q.Source2(), indent+1)
+	case q1i:
+		return strategy(q.Source(), indent) + "\n" +
+			in + cost + q.String()
+	default:
+		return in + cost + q.String()
+	}
+}
+
+// Strategy2 is like Strategy but without the cost/size estimates
+// so it is more stable for tests
+func Strategy2(q Query) string {
+	return strategy2(q, 0)
+}
+
+func strategy2(q Query, indent int) string { // recursive
+	in := strings.Repeat(indent1, indent)
+	switch q := q.(type) {
+	case *Sort:
+		if q.String() == "" {
+			return strategy2(q.Source(), indent)
+		} else {
+			return strategy2(q.Source(), indent) + "\n" +
+				in + q.String()
+		}
+	case q2i:
+		return strategy2(q.Source(), indent+1) + "\n" +
+			in + q.String() + "\n" +
+			strategy2(q.Source2(), indent+1)
+	case q1i:
+		return strategy2(q.Source(), indent) + "\n" +
+			in + q.String()
+	default:
+		return in + q.String()
+	}
 }
